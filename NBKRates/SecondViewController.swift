e02d0df868ca8941ec2ffc835180256a8aa9b22b
Trayerbee <betrayer@legion.zone>
2017-05-07 10:01:28 +0600

    Initial Commit

---
+++ NBKRates/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NBKRates
+//
+//  Created by Betrayer Stromrage on 5/7/17.
+//  Copyright © 2017 Trayerbee Development. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
